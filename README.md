77b5b56844440b83ec0bc48c861f15e76928e0fd
Tracey Jaquith <tracey_pooh@yahoo.com>
2020-05-08 14:29:16 -0700

    start of "spelling bee" knock off

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # word-salad
 
+```bash
+
+wget -qO- 'http://app.aspell.net/create?max_size=60&spelling=US&max_variant=0&diacritic=strip&download=wordlist&encoding=utf-8&format=inline' >| words-scowl.txt
+
+
+# NO lame 's variants
+# remove preamble
+# lowercase
+# 4+ letter words only
+egrep -v "'s$" words-scowl.txt \
+  |tr A-Z a-z \
+  |fgrep -A100000 -- --- \
+  |fgrep -v -- --- \
+  |tr A-Z a-z \
+  |egrep '^....' \
+  |sort -u -o words.txt
+
+```
---
+++ pick-letters.js
@@ -0,0 +1,124 @@
+#!/usr/bin/env node
+
+import { execSync } from 'child_process'
+import { runInContext } from 'vm'
+
+const SIZE = 7
+
+const log = console.log.bind(console)
+
+// letters to count of their tiles in scrabble (excluding blank tile ;-)
+const LETTERS = {
+  a: 9,
+  b: 2,
+  c: 2,
+  d: 4,
+  e: 12,
+  f: 2,
+  g: 3,
+  h: 2,
+  i: 9,
+  j: 1,
+  k: 1,
+  l: 4,
+  m: 2,
+  n: 6,
+  o: 8,
+  p: 2,
+  q: 1,
+  r: 6,
+  s: 4,
+  t: 6,
+  u: 4,
+  v: 2,
+  w: 2,
+  x: 1,
+  y: 2,
+  z: 1,
+}
+
+function random_letter() {
+  // The sum of all weights is 98
+  // The ratio of each weight to that sum is the frequency of the letter.
+  // For 12 E tiles, E’s frequency is 12/98, or 12.2%.
+  const total_weight = Object.values(LETTERS).reduce((tot, num) => tot + num)
+
+  let random = Math.floor(Math.random() * total_weight)
+
+  for (const [letter, count] of Object.entries(LETTERS)) {
+    // log({ letter, count, random })
+    random -= count
+
+    if (random < 0)
+      return letter
+  }
+}
+
+
+function create() {
+  // now pick letters
+  //   we have to include at least 1 vowel
+  //   we never want repeats or more than 2 vowels ;-)
+  let letters = []
+  let vowels = 0
+  while (letters.length < SIZE  ||  !vowels) {
+    const letter = random_letter()
+
+    // ensure 1 or 2 vowels only
+    const vowel = ['a', 'e', 'i', 'o', 'u'].includes(letter)
+    if (vowel) {
+      vowels += 1
+      if (vowels > 2)
+        continue
+    }
+
+    if (letter === 'q') {
+      // no Q w/o U
+      if (vowels > 2)
+        continue
+      letters.push('u')
+      vowels += 1
+    }
+
+    letters.push(letter)
+    // filter through Set to dedupe
+    letters = [...new Set(letters)]
+    // log({ letters })
+  }
+
+  // The only reason there are more than SIZE letters is took awhile to get a vowel (at the end).
+  // So shrink the front
+  while (letters.length > SIZE)
+    letters.shift()
+
+  // now pick the central letter
+  const consonants = letters.filter((e) => !['a', 'e', 'i', 'o', 'u'].includes(e))
+
+  const center = consonants[Math.floor(Math.random() * consonants.length)]
+  // log({ consonants, center, letters })
+
+
+  // now filter words dictionary to just the words made up of the limited letters,
+  // where each word _additionally_ has to contain center letter.
+  const cmd = `egrep '^[${letters.join('')}]+$' words.txt |fgrep ${center}`
+  // log({ cmd })
+  const words = execSync(cmd).toString().trimEnd().split('\n')
+
+  // find the words that have _all letters_ in them
+  const alls = []
+  for (const word of words) {
+    if ([...new Set(word.split(''))].join('').length === SIZE)
+      alls.push(word)
+  }
+
+  return { letters, center, words, alls }
+}
+
+
+let puzzle
+do {
+  puzzle = create()
+  process.stdout.write('.')
+} while (!puzzle.alls.length)
+
+log({ puzzle })
